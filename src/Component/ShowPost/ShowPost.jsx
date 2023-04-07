8bf05e625ac1d490ad9891b5694b4b3d29eb69dd
Progga Roy~ <proggaroy2000@gmail.com>
2023-04-07 13:26:41 -0700

    evenHandler use for show detail post and use navigate

--- src/Component/ShowPost/ShowPost.jsx
+++ src/Component/ShowPost/ShowPost.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import SinglePost from '../SinglePost/SinglePost';
 
 const ShowPost = () => {
     const showPost = useLoaderData()
-
+    const navigate = useNavigate()
+     const handleToBack =()=>{
+       navigate(-1)
+     }
     return (
         <div>
             <h3>Title: {showPost.title}</h3>
             <p>Id:{showPost.id}</p>
             <p> userId:{showPost.userId}</p>
             <p>Body: {showPost.body}</p>
+            <button onClick={handleToBack}>Back to post</button>
         </div>
     );
 };
--- src/Component/SinglePost/SinglePost.jsx
+++ src/Component/SinglePost/SinglePost.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './SinglePost.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SinglePost = (props) => {
-    const {userId,id}= props.singlePost
+    const {userId,id}= props.singlePost;
+    const navigate = useNavigate()
+     const handleNavigation=() =>{
+      navigate(`/post/${id}`)
+     }
     return (
         <div className='s-post'>
           <button>USERID :{userId} <Link to={`/post/${id}`}>show detail</Link></button>
+          <button onClick={handleNavigation}>With Handle Navigation</button>
           
         </div>
     );
